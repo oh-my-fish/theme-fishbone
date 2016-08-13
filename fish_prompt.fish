2610399ebb5ff0a0ebfc54fa2663cb5f96bff6bd
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2016-08-12 23:19:46 -0300

    Created the fish_prompt function to customize fish shell prompt

---
+++ fish_prompt.fish
@@ -0,0 +1,37 @@
+
+
+function fish_prompt -d "Fishbone custom prompt"
+
+    # Keep the command executed status
+    set --local last_status $status
+
+    show_path
+    show_status $last_status
+end
+
+
+function show_path -d "Prints current directory abbreviated"
+
+    set_color blue
+    echo -en "["
+
+    set_color yellow
+    echo -en (prompt_pwd)
+
+    set_color blue
+    echo -en "] "
+end
+
+
+function show_status -a last_status -d "Prints red/grey colon based on status"
+
+    set --local current_color grey
+
+    if [ $last_status -ne 0 ]
+        set current_color red
+    end
+
+    set_color $current_color
+    echo -en ": "
+    set_color normal
+end
